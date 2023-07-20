9a4c6ac71afacdaa966a83acfca5d050c616cee6
Rahul Mallick <rahulmallick1126@gmail.com>
2023-07-20 15:55:20 +0530

    added authentication using jwt

--- server/server.js
+++ server/server.js
@@ -1,11 +1,27 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const User = require("./models/user.model");
+const userRoute = require("./routes/user.route");
 const port = process.env.PORT;
+
+// connection to the database
+// mongoose
+//   .connect("mongodb://localhost/users_db", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log(`Db connected successfully`);
+//   })
+//   .catch((error) => {
+//     throw error;
+//   });
 
 const app = express();
 
-app.use("/user", userRoute);
+app.use(express.json());
+app.use("/", userRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
