08e9a74ea2f074b8ebf729152c2a09344f1f872d
Rahul Mallick <rahulmallick1126@gmail.com>
2023-07-20 15:53:49 +0530

    updated user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,23 +1,31 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const UserSchema = mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
+const UserSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (value) => validator.isEmail(value),
+        message: "Invalid email format",
+      },
+    },
+    age: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  confirmPasword: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", UserSchema);
-model.exports = User;
+module.exports = User;
